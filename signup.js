589248c9c967f94666cbc247cd61bd9a41b69704
AmrHishamFarouk <hishamamr121@gmail.com>
2024-02-10 00:16:28 +0200

    Add files via upload

--- signup.js
+++ signup.js
@@ -1,1 +1,14 @@
-let btn = document.querySelector(button);
+let btn = document.querySelector('.upload');
+let pass1 = document.querySelector('#password');
+let pass2 = document.querySelector('#repassword');
+let allpass = document.querySelectorAll('.error');
+
+
+btn.addEventListener('clicked',()=>{checking()});
+
+
+function checking(){
+    allpass.forEach(e => {
+        e.innerHTML = pass1.value == pass2.value ? 'matching' : 'not-matching';      
+    });
+}
